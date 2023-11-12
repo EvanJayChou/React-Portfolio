57333a69eca5cc5798bbed862d0c23f30cbab82c
EvanJayChou <evan.chou@live.com>
2023-11-12 01:30:42 -0800

    Updated welcome section

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ function App() {
   return (
     <>
       <div id='topbar'>
-      <h1 id='topleft'>Evan Chou</h1>
-      <ul id='sections'>
-        <li>About</li>
-        <li>Skills</li>
-        <li>Portfolio</li>
-        <li>Education</li>
-        <li>Contacts</li>
-      </ul>
-      </div><div id='welcome'>
+        <h1 id='topleft'>Evan Chou</h1>
+        <ul id='sections'>
+          <li><a href='#about'>About</a></li>
+          <li><a href='#skills'>Skills</a></li>
+          <li><a href='#portfolio'>Portfolio</a></li>
+          <li><a href='#education'>Education</a></li>
+          <li><a href='#contacts'>Contacts</a></li>
+        </ul>
+      </div>
+      <div id='welcome'>
         <h1 id='welcometitle'>Evan J. Chou</h1>
         <h2 id='jobtitle'>Software Engineer, Data Scientist, Student Researcher</h2>
-      </div><div id='about'>
+      </div>
+      <div id='about'>
         <h1>About:</h1>
         <p id='descrip'>
           Hello, my name is Evan Chou, and I am an avid and inquisitive college student developing skills and experience in software engineering and data science.
@@ -25,15 +27,17 @@ function App() {
       </div>
       <div id='skills'>
         <h1>Skills:</h1>
-        <ul id='skilllist'>
+        <ul>
           <li>Java</li>
           <li>JavaScript</li>
           <li>HTML</li>
           <li>CSS</li>
           <li>React</li>
         </ul>
+      </div>
+      <div id='skills'>
         <h1>Developer Tools:</h1>
-        <ul id='tools'>
+        <ul>
           <li>Eclipse IDE</li>
           <li>Visual Studio Code</li>
           <li>GitHub</li>
@@ -48,12 +52,13 @@ function App() {
       </div>
       <div id='education'>
         <h1>Education:</h1>
-        <h2>Pasadena City College</h2>
-        <h3>Associate's in Science: Electrical Engineering</h3>
-        <h2>University of California:</h2>
-        <h3>Bachelor's in Science: Electrical Engineering and Computer Sciences</h3>
-        <h2>University of California: San Diego</h2>
-        <h3>Master's in Science: Data Science</h3>
+          <h2>Pasadena City College (May 2023 - June 2025)</h2>
+            <h3>Associate's in Science: Electrical Engineering</h3>
+            <h3>Associate's in Arts: Engineering and Technologies</h3>
+          <h2>University of California: Berkeley (September 2025 - June 2027)</h2>
+            <h3>Bachelor's in Science: Electrical Engineering and Computer Sciences</h3>
+          <h2>University of California: San Diego (September 2027 - June 2029)</h2>
+            <h3>Master's in Science: Data Science</h3>
       </div>
       <div id='contacts'>
         <h1>Contact Me!</h1>
